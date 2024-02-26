5eb40f45e84ca51e68d7ed557ba1963025d3956a
Robb Corbett <rcorbett@wearescs.com>
2024-02-26 15:16:51 -0500

    pass image obj

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
-export default function Logo({source}) {
-    console.debug(source);
+export default function Logo({image}) {
+    console.debug(image);
     return (
-        <img className="h-9 p-1" src={source} alt="React" />
+        <img className="h-9 p-1" src={image.source} alt={image.alt} />
     );
 }
