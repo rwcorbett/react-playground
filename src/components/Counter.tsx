475af85b102c5224d4cc56961fe0d2d1f89dfe1d
rwcorbett <robb.corbett@gmail.com>
2024-03-05 08:19:32 -0500

    adding persistent session example

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+    const [totalCount, setTotalCount] = useState(0);
     const hoverHandler = () => {
         let newCount = count + 1;
         setCount(newCount);
+        setTotalCount(newCount);
     }
+
+    useEffect(() => {
+        const getLastCount = parseInt(sessionStorage.getItem('totalCount') ?? '0');
+        const newCount = count + getLastCount;
+        console.info(`storing new totalCount: ${count} + ${getLastCount} = ${newCount} `);
+        sessionStorage.setItem('totalCount', newCount.toString());
+    }, [totalCount])
+
     return (
         <div className="counter">
-            <p>Counter: {count}</p>
+            <p>Counter: {count} | Total hovers: {totalCount} </p>
             <div className="counter-box" onMouseEnter={hoverHandler}>
                 <p>Hover me to increase count!</p>
             </div>
